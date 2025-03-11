1a21ecd55b70d93c98ad3f9ccbaa58fe9f4b0867
Evan Bee <evan.huizinga@torontofilmschool.ca>
2025-03-11 17:47:12 -0400

    crow turret started
    follow hisham recordings to finish (seePlayer)

--- Assets/Scripts/Enemies/CrowTurretShoot.cs
+++ Assets/Scripts/Enemies/CrowTurretShoot.cs
@@ -3,19 +3,13 @@ using UnityEngine;
 [RequireComponent (typeof(BoxCollider2D))]
 public class CrowTurretShoot : MonoBehaviour
 {
-    //visibility variables
-    //public 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //if (
-        //anim.SetTrigger("seePlayer");
 
     }
 }
