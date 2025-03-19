d2aa619e2967f3f1d7901f3c7c013d45877605ff
Evan Bee <evan.huizinga@torontofilmschool.ca>
2025-03-19 18:17:24 -0400

    rats have been given the gift of locomotion
    rats now patrol back and forth in a set area

---
+++ Assets/Scripts/Enemies/PatrolEnemy.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody2D))]
+public class PatrolEnemy : Enemy
+{
+    Rigidbody2D rb;
+
+    [SerializeField] private float xVel;
+    protected override void Start()
+    {
+        base.Start();
+
+        rb = GetComponent<Rigidbody2D>();
+        rb.sleepMode = RigidbodySleepMode2D.NeverSleep;
+
+        if (xVel <= 0) xVel = 3;
+    }
+
+    public override void TakeDamage(int DamageValue, DamageType damageType = DamageType.Default)
+    {
+        if (damageType == DamageType.JumpedOn)
+        {
+            anim.SetTrigger("Death");
+            Destroy(transform.parent.gameObject, 0.5f);
+            return;
+        }
+
+        base.TakeDamage(DamageValue, damageType);
+    }
+    void Update()
+    {
+        AnimatorClipInfo[] curPlayingClips = anim.GetCurrentAnimatorClipInfo(0);
+        if (curPlayingClips[0].clip.name.Contains("RatWalk"))
+            rb.linearVelocity = (sr.flipX) ? new Vector2(-xVel, rb.linearVelocityY) : new Vector2(xVel, rb.linearVelocityY);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Barrier"))
+        {
+            sr.flipX = !sr.flipX;
+        }
+    }
+}
