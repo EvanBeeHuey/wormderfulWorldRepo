8073301593b721a17cf7d0d6b4628a1871148f6c
Evan Bee <evan.huizinga@torontofilmschool.ca>
2025-03-23 19:18:09 -0400

    tried to add win lose screens

---
+++ Assets/Scripts/Mechanics/EndLevel.cs
@@ -0,0 +1,19 @@
+using UnityEditor.Search;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour
+{
+    [SerializeField] private Transform winPlat;
+    [SerializeField] private Transform LosePlat;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
